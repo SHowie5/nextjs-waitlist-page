8b5b6d2213ea71121b194ffac9c4f3e58ed748ab
Scott Howie <scotthowie5@gmail.com>
2025-02-07 10:09:45 +0000

    Edit waitlist page

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,21 @@ export default function Home() {
       <Toaster />
 
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="md:h-full h-80 bg-[#FCD0A1] relative overflow-hidden">
+        <div className="md:h-full h-80 relative overflow-hidden">
           <Image
-            src="/mobile-app.png"
-            alt="Mobile App Screenshots"
+            src="/garage_image.png"
+            alt="Garage image"
             fill
-            className="object-contain mt-8 md:mt-24 px-14 object-bottom"
+            className="object-cover object-bottom left-0"
           />
         </div>
 
         <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
           <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
-            Join the conversation: Secure your spot on our Chat App!
+            Empty Space? Future Income. Join Our Waitlist
           </h1>
           <p className="text-gray-500">
-            Join the waitlist to be notified when our app is available!
+            Join the waitlist to be notified when our site is up and running!
           </p>
 
           <EmailForm />
@@ -31,3 +31,4 @@ export default function Home() {
     </>
   );
 }
+
